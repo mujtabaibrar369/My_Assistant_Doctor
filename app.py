77e4c6c340a2a6ce0c4d14569b49ff8182306ed2
mujtabaibrar369 <your-email@example.com>
2025-03-19 00:02:23 +0500

    memory fixes

--- app.py
+++ app.py
@@ -8,6 +8,14 @@ import torch.nn as nn
 from torchvision import transforms, models
 from PIL import Image
 
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+    except RuntimeError as e:
+        print(e)
+        
 # Initialize Flask app
 app = Flask(__name__)
 
